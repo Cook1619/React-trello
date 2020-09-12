8d3bbb91383ed8c1903603f3c0e93b0fdf2bb81f
Cook1619 <cook6348@gmail.com>
2020-09-12 10:50:13 -0500

    First commmit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// Components
+import Board from './components/Board';
+import Header from './components/Header';
+import Footer from './components/Footer';
+// Stylesheets
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Board />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
